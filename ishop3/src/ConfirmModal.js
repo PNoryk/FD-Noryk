101a0a9344fbfa1cb157b613d3430eba06a51331
Pavel Noryk <pavel.noryk@appsyoulove.com>
2022-10-30 18:56:43 +0300

    refactor: remove unnecessary state and modal show props

--- ishop3/src/ConfirmModal.js
+++ ishop3/src/ConfirmModal.js
@@ -1,20 +1,12 @@
 import { Component } from "react";
 
 export default class ConfirmModal extends Component {
-  componentDidUpdate = (prevProps, prevState, snapshot) => {
-    if (!prevProps.show) {
-      this.props.onShow();
-    }
-  }
+  componentDidMount = () => this.props.onMounted();
 
   render = () => {
     let text = this.props.children;
     return (
-      <div
-        className={"modal fade" + (this.props.show ? " show" : "")}
-        style={this.props.show ? { display: "block" } : {}}
-        tabIndex="-1"
-      >
+      <div className="modal fade show d-block" tabIndex="-1">
         <div className="modal-dialog">
           <div className="modal-content">
             <div className="modal-header">
@@ -24,23 +16,23 @@ export default class ConfirmModal extends Component {
                 className="btn-close"
                 data-bs-dismiss="modal"
                 aria-label="Close"
-                onClick={this.props.onClose}
+                onClick={ this.props.onClose }
               />
             </div>
-            <div className="modal-body">{text}</div>
+            <div className="modal-body">{ text }</div>
             <div className="modal-footer">
               <button
                 type="button"
                 className="btn btn-secondary"
                 data-bs-dismiss="modal"
-                onClick={this.props.onClose}
+                onClick={ this.props.onClose }
               >
                 Close
               </button>
               <button
                 type="button"
                 className="btn btn-primary"
-                onClick={this.props.onConfirm}
+                onClick={ this.props.onConfirm }
               >
                 Confirm
               </button>
--- ishop3/src/Shop.js
+++ ishop3/src/Shop.js
@@ -8,8 +8,8 @@ export const Shop = ({ products: initialProducts, headings }) => {
   let [ products, setProducts ] = useState(initialProducts);
   let [ selectedId, setSelectedId ] = useState(null);
   let [ showModal, setShowModal ] = useState(false);
-  let [ productToRemove, setProductToRemove ] = useState(null);
   let removeCountRef = useRef(null);
+  let [ countToRemove, setCountToRemove ] = useState(1);
   let [ hasError, setHasError ] = useState(false);
 
   let [ isProductFormOpened, setIsProductFormOpened ] = useState(false);
@@ -18,11 +18,7 @@ export const Shop = ({ products: initialProducts, headings }) => {
   let createHeadings = () =>
     Object.values(headings).map((heading) => <th key={ heading }>{ heading }</th>);
 
-  const onShowModal = (id) => {
-    setProductToRemove(products.find(({ id: productId }) => productId === id));
-    setShowModal(true);
-    removeCountRef.current.value = 1;
-  };
+  let selectedProduct = products.find(({ id }) => id === selectedId);
 
   let createRows = () =>
     products.map((product) => (
@@ -47,7 +43,7 @@ export const Shop = ({ products: initialProducts, headings }) => {
         <button
           type="button"
           className="btn btn-danger ms-1"
-          onClick={ () => onShowModal(product.id) }
+          onClick={ () => setShowModal(true) }
           disabled={ isProductFormOpened }
         >
           remove
@@ -56,35 +52,29 @@ export const Shop = ({ products: initialProducts, headings }) => {
     ));
 
   let removeProducts = (count) => {
-    setProducts((previousProducts) => {
-      let removeProductId = productToRemove.id;
-      let selected = previousProducts.find(({ id }) => id === removeProductId);
-      let products;
-      if (!(selected.count - count)) {
-        products = previousProducts.filter(({ id }) => id !== removeProductId);
-      } else {
-        products = previousProducts.map((product) =>
-          product.id === removeProductId
-            ? { ...product, count: product.count - count }
-            : product
-        );
+    setProducts(() => {
+      let removeProductId = selectedProduct.id;
+      if (!(selectedProduct.count - count)) {
+        return products.filter(({ id }) => id !== removeProductId);
       }
-      return products;
+      return products.map((product) =>
+        product.id === removeProductId
+          ? { ...product, count: product.count - count }
+          : product
+      );
     });
-    setProductToRemove(null);
     setShowModal(false);
   };
 
   const onConfirm = () => {
-    let value = +removeCountRef.current.value;
-    if (value < 1 || value > productToRemove.count || value % 1 !== 0) {
+    let value = +countToRemove;
+    if (value < 1 || value > selectedProduct.count || value % 1 !== 0) {
       setHasError(true);
       return;
     }
     removeProducts(value);
   };
 
-  let selectedProduct = products.find(({ id }) => id === selectedId);
   let onProductFormSave = (product) => {
     let isNew = !("id" in product);
     if (isNew) {
@@ -122,32 +112,38 @@ export const Shop = ({ products: initialProducts, headings }) => {
           </button>
         </p>
       ) }
-      <ConfirmModal
-        show={ showModal }
-        onShow={ () => removeCountRef.current.focus() }
-        onClose={ () => {
-          setProductToRemove(null);
-          setShowModal(false);
-          removeCountRef.current.value = "";
-        } }
-        onConfirm={ onConfirm }
-      >
-        <p>How many { productToRemove?.name } would you like to remove?</p>
-        <input
-          className={ "form-control" + (hasError ? " is-invalid" : "") }
-          type="number"
-          min={ 1 }
-          max={ productToRemove?.count }
-          ref={ removeCountRef }
-          onChange={ () => setHasError(false) }
-        />
-        { hasError && (
-          <div className="invalid-feedback">
-            <div>Set a valid integer.</div>
-            <div>It should be between 1 and { productToRemove?.count }</div>
-          </div>
-        ) }
-      </ConfirmModal>
+      { selectedProduct && showModal && (
+        <ConfirmModal
+          onMounted={ () => {
+          } }
+          // onMounted={ () => {removeCountRef.current.focus();
+          //   console.log(removeCountRef.current);} }
+          onClose={ () => {
+            setShowModal(false);
+            setCountToRemove(1);
+          } }
+          onConfirm={ onConfirm }
+        >
+          <p>How many { selectedProduct.name } would you like to remove?</p>
+          <input
+            className={ "form-control" + (hasError ? " is-invalid" : "") }
+            type="number"
+            min={ 1 }
+            max={ selectedProduct.count }
+            value={ countToRemove }
+            onChange={ (e) => {
+              setCountToRemove(e.target.value);
+              setHasError(false);
+            } }
+          />
+          { hasError && (
+            <div className="invalid-feedback">
+              <div>Set a valid integer.</div>
+              <div>It should be between 1 and { selectedProduct.count }</div>
+            </div>
+          ) }
+        </ConfirmModal>
+      ) }
       { selectedProduct && <ProductDetail product={ selectedProduct }/> }
       { isProductFormOpened && (
         <ProductForm
