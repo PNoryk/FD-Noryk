4117938ab411db6353deb8ba7ee6f7ddc9cc08dd
Pavel Noryk <pavel.noryk@appsyoulove.com>
2022-10-30 13:32:08 +0300

    fix: br after the latest element

--- br2jsx/src/BR2JSX.js
+++ br2jsx/src/BR2JSX.js
@@ -1,5 +1,10 @@
 export const BR2JSX = ({ text }) => {
   let newArr = [];
-  text.split(/<br\s*\/?>/).forEach((el, i) => newArr.push(el, <br key={i} />));
-  return <div className="br2jsx">{ newArr }</div>;
+  text.split(/<br\s*\/?>/).forEach((el, i, { length }) => {
+    newArr.push(el);
+    if (i !== length - 1) {
+      newArr.push(<br key={i} />);
+    }
+  });
+  return <div className="br2jsx">{newArr}</div>;
 };
