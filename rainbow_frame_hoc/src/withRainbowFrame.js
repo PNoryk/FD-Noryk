82a6c41b372fca4380fe7fe4100664a923c96f39
Pavel Noryk <pavel.noryk@appsyoulove.com>
2022-10-31 22:52:12 +0300

    refactor: simplify HOC

--- rainbow_frame_hoc/src/withRainbowFrame.js
+++ rainbow_frame_hoc/src/withRainbowFrame.js
@@ -1,13 +1,11 @@
-import { createElement } from "react";
-
 const makeDiv = (color, children) => (
   <div style={{ margin: "10px", border: `10px solid ${color}` }}>
     {children}
   </div>
 );
 
-export const withRainbowFrame = (colors) => (component) => (props) =>
+export const withRainbowFrame = (colors) => (Component) => (props) =>
   colors.reduce(
     (prev, color) => makeDiv(color, prev),
-    createElement(component, props)
+    <Component {...props} />
   );
