6237ccfc4e98674e49158ff492cb3708379feb6d
Pavel Noryk <pavel.noryk@appsyoulove.com>
2022-10-31 09:28:51 +0300

    refactor: move logic to withRainbowFrame

--- rainbow_frame_hoc/src/withRainbowFrame.js
+++ rainbow_frame_hoc/src/withRainbowFrame.js
@@ -1,5 +1,13 @@
-import { RainbowFrame } from "./RainbowFrame";
 import { createElement } from "react";
 
+const makeDiv = (color, children) => (
+  <div style={{ margin: "10px", border: `10px solid ${color}` }}>
+    {children}
+  </div>
+);
+
 export const withRainbowFrame = (colors) => (component) => (props) =>
-  <RainbowFrame colors={colors}>{createElement(component, props)}</RainbowFrame>;
+  colors.reduce(
+    (prev, color) => makeDiv(color, prev),
+    createElement(component, props)
+  );
