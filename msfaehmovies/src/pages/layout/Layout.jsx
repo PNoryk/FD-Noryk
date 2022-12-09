1c8751c03e78a04c1ade6580ec03945b017ffb57
Pavel Noryk <pavel.noryk@appsyoulove.com>
2022-12-09 23:20:31 +0300

    feat: add header sidebar

--- msfaehmovies/src/pages/layout/Layout.jsx
+++ msfaehmovies/src/pages/layout/Layout.jsx
@@ -1,7 +1,13 @@
 import "./styles.scss";
 
+import classNames from "classnames";
 import { useState } from "react";
 
+import { ReactComponent as BookmarkIcon } from "@/assets/icons/Bookmark.svg";
+import { ReactComponent as FlameIcon } from "@/assets/icons/Flame.svg";
+import { ReactComponent as HomeIcon } from "@/assets/icons/Home.svg";
+import { ReactComponent as SearchIcon } from "@/assets/icons/Search.svg";
+import { ReactComponent as SettingIcon } from "@/assets/icons/Setting.svg";
 import logoPath from "@/assets/logo.svg";
 
 export const Layout = () => {
@@ -9,20 +15,59 @@ export const Layout = () => {
 
   return (
     <>
-      <header className="header">
-        <img className="header__image" src={logoPath} alt="Logo" />
-        <button
-          className="header__menu-button menu-button"
-          aria-expanded={isSidebarOpened}
-          onClick={() => setIsSidebarOpened(!isSidebarOpened)}
+      <div className="container">
+        <header
+          className={classNames(
+            "container__main",
+            {
+              "container__main--aside-opened": isSidebarOpened,
+            },
+            "header"
+          )}
         >
-          <span className="visually-hidden">menu</span>
-          <span className="menu-button__item"></span>
-          <span className="menu-button__item"></span>
-          <span className="menu-button__item"></span>
-        </button>
-        <input className="header__search search" placeholder="Search" type="text"/>
-      </header>
+          <img className="header__image" src={logoPath} alt="Logo" />
+          <button
+            type="button"
+            className="header__menu-button menu-button"
+            aria-expanded={isSidebarOpened}
+            onClick={() => setIsSidebarOpened(!isSidebarOpened)}
+          >
+            <span className="visually-hidden">menu</span>
+            <span className="menu-button__item"></span>
+            <span className="menu-button__item"></span>
+            <span className="menu-button__item"></span>
+          </button>
+          <div className="header__search search">
+            <input className="search__input" placeholder="Search" type="text" />
+            <button type="button" className="search__button">
+              <span className="visually-hidden">search filter button</span>
+              <SearchIcon />
+            </button>
+          </div>
+        </header>
+        <aside
+          className={classNames("navbar", "container__sidebar", {
+            "container__sidebar--opened": isSidebarOpened,
+          })}
+        >
+          <nav>
+            <ul className="navbar__list">
+              <li className="navbar__item">
+                <HomeIcon /> Home
+              </li>
+              <li className="navbar__item">
+                <FlameIcon /> Trends
+              </li>
+              <li className="navbar__item">
+                <BookmarkIcon /> Favorites
+              </li>
+              <li className="navbar__item">
+                <SettingIcon /> Settings
+              </li>
+            </ul>
+          </nav>
+        </aside>
+      </div>
     </>
   );
 };
--- msfaehmovies/vite.config.js
+++ msfaehmovies/vite.config.js
@@ -2,10 +2,11 @@ import eslintPlugin from "@nabla/vite-plugin-eslint";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
+import viteSvgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslintPlugin()],
+  plugins: [react(), viteSvgr(), eslintPlugin()],
   resolve: {
     alias: {
       "@/": `${path.resolve(__dirname, "src")}/`,
