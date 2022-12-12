3fdc004a0df80f423b36040362f3b7373f2ccc3c
Pavel Noryk <pavel.noryk@appsyoulove.com>
2022-12-13 02:01:15 +0300

    feat: add users login/logout dropdown

--- msfaehmovies/src/pages/layout/Layout.jsx
+++ msfaehmovies/src/pages/layout/Layout.jsx
@@ -3,6 +3,7 @@ import "./styles.scss";
 import classNames from "classnames";
 import { useState } from "react";
 
+import { ReactComponent as Arrow } from "@/assets/icons/Arrow.svg";
 import { ReactComponent as BookmarkIcon } from "@/assets/icons/Bookmark.svg";
 import { ReactComponent as FlameIcon } from "@/assets/icons/Flame.svg";
 import { ReactComponent as HomeIcon } from "@/assets/icons/Home.svg";
@@ -12,6 +13,7 @@ import logoPath from "@/assets/logo.svg";
 
 export const Layout = () => {
   let [isSidebarOpened, setIsSidebarOpened] = useState(false);
+  let [isDropDownOpened, setIsDropDownOpened] = useState(false);
 
   return (
     <>
@@ -25,14 +27,14 @@ export const Layout = () => {
             <img className="header__image" src={logoPath} alt="Logo" />
             <button
               type="button"
-              className="header__menu-button menu-button"
+              className="header__menu-button icon-button header-menu"
               aria-expanded={isSidebarOpened}
               onClick={() => setIsSidebarOpened(!isSidebarOpened)}
             >
               <span className="visually-hidden">menu</span>
-              <span className="menu-button__item"></span>
-              <span className="menu-button__item"></span>
-              <span className="menu-button__item"></span>
+              <span className="header-menu__item"></span>
+              <span className="header-menu__item"></span>
+              <span className="header-menu__item"></span>
             </button>
             <div className="header__search search">
               <input
@@ -44,6 +46,25 @@ export const Layout = () => {
                 <span className="visually-hidden">search filter button</span>
                 <SearchIcon />
               </button>
+            </div>
+            <div
+              className={classNames("header__dropdown", "dropdown", {
+                "dropdown--opened": isDropDownOpened,
+              })}
+            >
+              <button
+                type="button"
+                className="dropdown__button dropdown-button"
+                onClick={() => setIsDropDownOpened(!isDropDownOpened)}
+              >
+                <span className="dropdown__button-rect icon-button">KK</span>
+                <span className="dropdown__button-text">Константин Константинопольский</span>
+                <Arrow className="dropdown__arrow" />
+              </button>
+              <ul className="dropdown__menu">
+                <li className="dropdown__menu-item">SignIn</li>
+                <li className="dropdown__menu-item">SignUp</li>
+              </ul>
             </div>
           </header>
         </div>
